af6e5bccc1b9e4a34ce05d50207410be1d79764a
zorba11 <zorbathegreek111@gmail.com>
2019-03-18 15:35:06 -0400

    commit before build

---
+++ components/myHeader.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-vr";
+
+const MyHeader = ({ currentPlanet }) => (
+  <View style={styles.header}>
+    <Text style={styles.headerTitle}>Alen George</Text>
+    <Text style={styles.headerSubtitle}>
+      Look Left to Know My {currentPlanet}
+    </Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  header: {
+    position: "absolute",
+    width: 600,
+    transform: [{ translate: [-300, 260, 0] }]
+  },
+  headerTitle: {
+    fontSize: 60,
+    textAlign: "center",
+    color: "#fff"
+  },
+  headerSubtitle: {
+    fontSize: 30,
+    textAlign: "center",
+    color: "#fff"
+  }
+});
+
+export default MyHeader;
---
+++ data/planets.js
@@ -0,0 +1,50 @@
+const planets = {
+  Skills: {
+    info: [
+      { label: "Front-end: HTML, CSS, SASS, React & Redux", value: 7 },
+      { label: "Back-end: Java, Ajax, Node & Express, SQL", value: 6 },
+      { label: "Libraries & Design: jQuery, Three.js, Photoshop", value: 7 }
+    ],
+    description:
+      "Once he started taking web and mobile development development seriously, he spent more time focusing on digesting the Object Oriented and Functional Programming Principles. Also, as a person working with the web, naturally he did fell in love with JavaScript. As a person who learned the verbose nature of Java, JavaScript is sweet. Also, he do enjoy exploring various JS libraries. Also, recently he did start working with Android studio and he is loving it's easiness too."
+  },
+  // Neptune: {
+  //   info: [
+  //     {label: 'Mean distance from the sun (AU)', value: 30.06},s
+  //     {label: 'Diameter relative to Skills (km)', value: 3.88},
+  //     {label: 'Mass relative to Skills', value: 17.23}
+  //   ],
+  //   info: "Neptune is the eighth and farthest known planet from the Sun in the Solar System. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. Neptune is 17 times the mass of Skills and is slightly more massive than its near-twin Uranus, which is 15 times the mass of Skills and slightly larger than Neptune."
+  // },
+  // Jupiter: {
+  //   info: [
+  //     {label: 'Mean distance from the sun (AU)', value: 5.203},
+  //     {label: 'Diameter relative to Skills (km)', value: 11.19},
+  //     {label: 'Mass relative to Skills', value: 317.8}
+  //   ],
+  //   info: "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a giant planet with a mass one-thousandth that of the Sun, but two and a half times that of all the other planets in the Solar System combined. Jupiter and Experience are gas giants; the other two giant planets, Uranus and Neptune are ice giants."
+  // },
+  Bio: {
+    info: [
+      { label: "Birth Place: Kerala, India", value: null },
+      { label: "Age: 25 ", value: null },
+      {
+        label: "Languages: English, Malayalam, Hindi, Tamil(spoken)",
+        value: null
+      }
+    ],
+    description:
+      "As a person coming from South India, the hub of Indian Software Industry, it was natural for him to choose development. Before choosing programming, he was working as a Freelance AdFilm maker and a shortfilm director, which he believe had definitely improved his design senses."
+  },
+  Experience: {
+    info: [
+      { label: "PRO/WebDesigner : Aquamarine Hospitality", value: null },
+      { label: "Portfolio : www.alengeorge.com", value: null },
+      { label: "AdFilm maker : Freelance", value: null }
+    ],
+    description:
+      "In 2015 with self-thought HTML, CSS and JS he was able to build the initial website of the company he worked for(It was a simple static site).Later on, his job was to make advertisements for some local events and also during that period he was able conceive and direct a Short Film-'Portrait' which won the award for best film in some film festivals , also 'Portrait' was screened at ALIIFF International Film Festival. Later on, realizing the immense possibilities of being a programmer, he decided to pursue coding."
+  }
+};
+
+export default planets;
